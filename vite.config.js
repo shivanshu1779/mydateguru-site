72ff995281cd994162836c3ac863c5562b12dbf5
Shivanshu Sharma <132002474+shivanshu1779@users.noreply.github.com>
2025-10-06 15:40:17 +0530

    delete old files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Simple Vite config; no path aliases used to avoid build errors.
-export default defineConfig({
-  plugins: [react()],
-})
